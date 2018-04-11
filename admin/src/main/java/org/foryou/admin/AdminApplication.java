934441df29f0f32d574fed5365e6152dbac078ab
L <1016823429@qq.com>
2018-04-11 22:12:45 +0800

    外部配置

--- admin/src/main/java/org/foryou/admin/AdminApplication.java
+++ admin/src/main/java/org/foryou/admin/AdminApplication.java
@@ -4,15 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 @EnableJpaRepositories("org.foryou.jpa")
 @EntityScan("org.foryou.dao")
+@PropertySource(value = {"file:/application.properties"})
 public class AdminApplication {
 
     public static void main(String[] args) {
