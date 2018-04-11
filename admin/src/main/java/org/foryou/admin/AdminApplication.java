118aeff7464d105bdf22677da6d90edd47381b9a
zhangning <1016823429@qq.com>
2018-04-11 14:02:34 +0800

    添加jpa redis

--- admin/src/main/java/org/foryou/admin/AdminApplication.java
+++ admin/src/main/java/org/foryou/admin/AdminApplication.java
@@ -2,11 +2,28 @@ package org.foryou.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.foryou.jpa")
+@EntityScan("org.foryou.dao")
 public class AdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setConnectionFactory(factory);
+        return template;
+    }
 }
