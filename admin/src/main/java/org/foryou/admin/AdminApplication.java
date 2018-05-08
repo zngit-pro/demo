91fa03afd526835e79991d98c8a5dfacbc14b105
zhangning <1016823429@qq.com>
2018-05-08 17:04:06 +0800

    log request

--- admin/src/main/java/org/foryou/admin/AdminApplication.java
+++ admin/src/main/java/org/foryou/admin/AdminApplication.java
@@ -6,17 +6,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableJpaRepositories(value = "org.foryou.admin.jpa", repositoryBaseClass = CustomRepositoryImpl.class)
 @EntityScan("org.foryou.dao")
 @MapperScan("org.foryou.admin.mybatis")
 @PropertySource(value = {"file:/etc/demo/admin/application.properties"})
+@Configuration
 public class AdminApplication {
 
     public static void main(String[] args) {
@@ -29,5 +32,16 @@ public class AdminApplication {
         template.setKeySerializer(new StringRedisSerializer());
         template.setConnectionFactory(factory);
         return template;
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludeHeaders(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
     }
 }
